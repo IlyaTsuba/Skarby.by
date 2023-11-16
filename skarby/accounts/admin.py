f963d4ec1d066b314cd72144c5d93618eeb65514
IlyaTsuba <ilyatsubablr@gmail.com>
2023-11-16 16:17:30 +0300

    registered in admin panel accounts and photos

--- skarby/accounts/admin.py
+++ skarby/accounts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from accounts.models import Account, Photos
 
-# Register your models here.
+
+class PhotoInline(admin.TabularInline):
+    model = Photos
+    extra = 1
+
+
+class AccountAdmin(admin.ModelAdmin):
+    inlines = [PhotoInline]
+
+
+admin.site.register(Account, AccountAdmin)
+admin.site.register(Photos)
