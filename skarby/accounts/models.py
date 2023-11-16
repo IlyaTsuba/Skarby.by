e3bb22d0d68e6c188ba6cfd1cf0e367aee5a6d5b
IlyaTsuba <ilyatsubablr@gmail.com>
2023-11-16 16:10:32 +0300

    added models for account and photo

--- skarby/accounts/models.py
+++ skarby/accounts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    name = models.CharField(max_length=70, verbose_name='Імя/Назва')
+    description = models.TextField()
+    instagram = models.CharField(max_length=50, null=True, blank=True)
+    telegram = models.CharField(max_length=50, blank=True, null=True)
+    avatar = models.ImageField(upload_to='media/photos/%Y/%m/%d/', verbose_name='Аватар')
+
+
+class Photos(models.Model):
+    accounts = models.ForeignKey(Account, on_delete=models.CASCADE, verbose_name='Аккаўнт')
+    photo = models.ImageField(upload_to='media/photos/%Y/%m/%d/', verbose_name='Фота')
+
