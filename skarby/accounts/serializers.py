08380d95851448642c23604a29ebf9d43ed23031
IlyaTsuba <ilyatsubablr@gmail.com>
2023-11-24 18:05:50 +0300

    added "add account to saved_accounts"

--- skarby/accounts/serializers.py
+++ skarby/accounts/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from accounts.models import Account
+from accounts.models import Account, SavedAccount
 
 
 class AccountSerializer(ModelSerializer):
     class Meta:
         model = Account
         fields = ('account_slug', 'name', 'description', 'instagram', 'telegram', 'avatar', 'category')
+
+
+class SavedAccountSerializer(ModelSerializer):
+    class Meta:
+        model = SavedAccount
+        fields = ('user', 'account', 'created_at')
--- skarby/accounts/views.py
+++ skarby/accounts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
 
-from accounts.models import Account
-from accounts.serializers import AccountSerializer
+from accounts.models import Account, SavedAccount
+from accounts.serializers import AccountSerializer, SavedAccountSerializer
 
 
 class AccountsListView(ListAPIView):
@@ -23,3 +25,28 @@ class AccountDetailView(APIView):
 
         return Response(serializer.data)
 
+
+# Save account functionality
+class SaveAccountView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, account_id):
+        account = get_object_or_404(Account, id=account_id)
+        user = self.request.user
+
+        saved_account, created = SavedAccount.objects.get_or_create(user=user, account=account)
+
+        if created:
+            return Response({'message': 'Account saved successfully.'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response({'message': 'Account already saved.'}, status=status.HTTP_200_OK)
+
+
+class SavedAccountsListView(APIView):
+    permission_classes = [IsAuthenticated, ]
+
+    def get(self, request):
+        user = self.request.user
+        saved_accounts = SavedAccount.objects.filter(user=user)
+        serializer = SavedAccountSerializer(saved_accounts, many=True)
+        return Response(serializer.data)
