0a13dcdbf2f43be21eab758eb98c70b70767d78f
IlyaTsuba <ilyatsubablr@gmail.com>
2023-11-26 17:15:05 +0300

    added delete account from saved

--- skarby/accounts/views.py
+++ skarby/accounts/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, DestroyAPIView
 
 from accounts.models import Account, SavedAccount
 from accounts.serializers import AccountSerializer, SavedAccountSerializer
@@ -18,6 +18,10 @@ class AccountsListView(ListAPIView):
 
 
 class AccountDetailView(APIView):
+    """
+    This view is to show exact Account details
+    """
+
     def get(self, request, account_slug):
         account = get_object_or_404(Account, account_slug=account_slug)
 
@@ -27,26 +31,35 @@ class AccountDetailView(APIView):
 
 
 # Save account functionality
-class SaveAccountView(APIView):
-    permission_classes = [IsAuthenticated]
+
+
+class SavedAccountsCreateDeleteView(APIView):
+    permission_classes = [IsAuthenticated, ]
 
     def post(self, request, account_id):
         account = get_object_or_404(Account, id=account_id)
-        user = self.request.user
+        user = request.user
 
         saved_account, created = SavedAccount.objects.get_or_create(user=user, account=account)
+        # get_or_create method creates object if object doesn't exist in saved and returns True, if not returns False
 
-        if created:
-            return Response({'message': 'Account saved successfully.'}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({'message': 'Account already saved.'}, status=status.HTTP_200_OK)
+        if created:  # True, new account saved
+            return Response({'message': 'Акаўнт захаваны'}, status=status.HTTP_201_CREATED)
+        else:  # False, account already exists in saved
+            return Response({'message': 'Акаўнт ужо захаваны'}, status=status.HTTP_200_OK)
+
+    def delete(self, request, account_id):
+        user = request.user
+        account_to_delete = get_object_or_404(SavedAccount, user=user, id=account_id)
+        account_to_delete.delete()
+        return Response({'message': 'Акаўнт быў выдалены'}, status=status.HTTP_204_NO_CONTENT)
 
 
 class SavedAccountsListView(APIView):
     permission_classes = [IsAuthenticated, ]
 
     def get(self, request):
-        user = self.request.user
+        user = request.user
         saved_accounts = SavedAccount.objects.filter(user=user)
         serializer = SavedAccountSerializer(saved_accounts, many=True)
         return Response(serializer.data)
