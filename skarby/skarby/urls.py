f4ffc17a46d1e7ec01d99b3eff1428ade0d863c2
IlyaTsuba <ilyatsubablr@gmail.com>
2023-12-01 12:32:49 +0300

    added swagger

--- skarby/skarby/urls.py
+++ skarby/skarby/urls.py
@@ -15,20 +15,41 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="skarby.by",
+        default_version='v1',
+        description="from belarusians with love",
+        license=openapi.License(name="BSD Lisence"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny, ],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', include('accounts.urls')),
+
+    # Debug toolbar
+
     path("__debug__/", include("debug_toolbar.urls")),
+
+    # Swagger
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 
 admin.site.site_header = 'Адмін-панэль'
 admin.site.index_title = 'Скарбы'
