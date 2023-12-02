1f9341449c0088e8fd791c233ca945b349277d61
IlyaTsuba <ilyatsubablr@gmail.com>
2023-12-02 22:38:10 +0300

    added docs to backend authentication

--- skarby/users/backends.py
+++ skarby/users/backends.py
@@ -4,15 +4,21 @@ from django.db.models import Q
 
 
 class CustomAuthenticationBackend(ModelBackend):
+    """
+    This custom backend allows users to log in, using both email and username
+    """
+
     def authenticate(self, request, email=None, password=None, **kwargs):
-        UserModel = get_user_model()
+
+        UserModel = get_user_model()  # get user model, in our case UserModel = CustomUser
 
         try:
             user = UserModel.objects.get(Q(email=email) | Q(username=email))
+            # identify user using email or username
         except UserModel.DoesNotExist:
             return None
 
-        if user.check_password(password):
+        if user.check_password(password):  # check password
             return user
 
     def get_user(self, user_id):
--- skarby/users/serializers.py
+++
@@ -1,2 +0,0 @@
-from djoser.email import ActivationEmail
-from django.utils.translation import gettext_lazy as _
